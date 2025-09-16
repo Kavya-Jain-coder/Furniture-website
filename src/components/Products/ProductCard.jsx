86dd72cb95c2e40fcdff385a29453c5aacde0476
Kavya-jain <kavyajain31052006@gmail.com>
2025-09-16 17:23:28 +0530

    ProductCard.jsx

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -18,15 +18,7 @@ const ProductCard = ({ data }) => {
                 alt="product-img"
                 className="h-[250px] w-[260px] bg-gradient-to-br from-[#fbf3e9] to-[#f3d9b0] rounded-4xl  object-contain"
               />
-              <div className="hidden group-hover:flex absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 h-full w-full text-center group-hover:backdrop-blur-sm justify-center items-center duration-200 rounded-full ">
-                <a href={data.href}>
-                  <Button
-                    text={"View Similar Products"}
-                    bgColor={"bg-[#f42c37]"}
-                    textColor={"text-white"}
-                  />
-                </a>
-              </div>
+              
             </div>
             <div className="leading-7 text-center">
               <h2 className="font-semibold">{data.title}</h2>
