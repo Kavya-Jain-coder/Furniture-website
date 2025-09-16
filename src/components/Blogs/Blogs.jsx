dab3d8152335a1c953eb31deafc1c1e1737aaf00
Kavya-jain <kavyajain31052006@gmail.com>
2025-09-16 17:21:41 +0530

    Blogs.jsx

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -8,7 +8,7 @@ const BlogData = [
   {
     title: "How to Choose the Perfect Sofa for Your Space",
     subtitle:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, odio mollitia. Possimus facere ab repudiandae laudantium aspernatur vero earum nobis.",
+      "Are you tired of scouring furniture stores and not finding the best sofa for your home? Look no further! Sleepyhead has revolutionized the way we shop for furniture, making it easier than ever to find exactly what you're looking for.In this blog post, we'll guide you through the process of finding the perfect sofa online – from selecting the right style and size to navigating delivery options. Get ready to sit back and relax on your dream couch! Staying indoors due to the pandemic has made us all reconsider how much we value comfort. We all love staying in our comfy pyjamas to attend meetings (yes, we know you do!).",
     published: "Jan 20, 2025 by Dilshad",
     image: Img1,
     aosDelay: "0",
@@ -16,7 +16,7 @@ const BlogData = [
   {
     title: "Top 5 Bedroom Design Trends of 2025",
     subtitle:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, odio mollitia. Possimus facere ab repudiandae laudantium aspernatur vero earum nobis.",
+      "Bedroom design trends are a tough sell: Your bedroom is your sanctuary, which is why people stick to soothing, neutral color palettes and crisp white bedding. But even if you cling to those restorative vibes, there's a world of design possibilities to unlock.The bedroom design trends for 2025 embrace subtle inflections of personality—think textured walls or lush, floor-to-ceiling curtains. Floral wallpapers and vintage touches are also on the rise. To help us navigate the top eight bedroom design trends emerging for 2025, we tapped a handful of expert designers to reveal what they're seeing in the industry.",
     published: "Jan 20, 2025 by Satya",
     image: Img2,
     aosDelay: "200",
@@ -24,7 +24,7 @@ const BlogData = [
   {
     title: "Maximizing Small Spaces with Smart Furniture",
     subtitle:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, odio mollitia. Possimus facere ab repudiandae laudantium aspernatur vero earum nobis.",
+      "Living in a small space can be challenging, especially when it comes to choosing the right furniture. Space saving furniture is not just a trend; it’s a necessity for those wanting to maximize their living area without sacrificing style. From multifunctional sofas to compact dining tables, there are countless solutions available that cater to small spaces. Let's explore some of the most effective space saving furniture options that can transform your home into a stylish and functional haven.",
     published: "Jan 20, 2025 by Sahil",
     image: Img3,
     aosDelay: "400",
@@ -39,7 +39,7 @@ const Blogs = () => {
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 gap-y-8 sm:gap-4 md:gap-7 ">
           {BlogData.map((data) => (
-            <a href="/blogs2">
+            
               <div
                 data-aos="fade-up"
                 data-aos-delay={data.aosDelay}
@@ -61,7 +61,7 @@ const Blogs = () => {
                   </p>
                 </div>
               </div>
-            </a>
+            
           ))}
         </div>
       </div>
